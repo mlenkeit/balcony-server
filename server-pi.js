1f8ba34560958cd08ccabf2967a4e81955af8c2b
Maximilian Lenkeit <maximilian.lenkeit@sap.com>
2017-08-08 08:36:40 +0200

    feat(capture-data): capture data from pi

--- server-pi.js
+++ server-pi.js
@@ -1,20 +1,28 @@
 'use strict';
 
 const assert = require('assert');
+const exec = require('./lib/util/exec');
 const fs = require('fs');
 const logger = require('heroku-logger');
 const path = require('path');
 const uuid = require('uuid/v4');
+const validateMeasurementObject = require('./lib/validation').validateMeasurementObject;
 const xsenv = require('@sap/xsenv');
 
 xsenv.loadEnv();
 
+const HTTP_REPO_URI = process.env.HTTP_REPO_URI;
+assert(HTTP_REPO_URI, 'missing env var HTTP_REPO_URI');
+const MONGODB_URI = process.env.MONGODB_URI;
+assert(MONGODB_URI, 'missing env var MONGODB_URI');
 const pumpPlugDeviceId = process.env.PLUG_DEVICE_ID_PUMP;
 assert(pumpPlugDeviceId, 'missing env var PLUG_DEVICE_ID_PUMP');
 const tomatoesPlugDeviceId = process.env.PLUG_DEVICE_ID_TOMATOES;
 assert(tomatoesPlugDeviceId, 'missing env var PLUG_DEVICE_ID_TOMATOES');
 const balconyPlugDeviceId = process.env.PLUG_DEVICE_ID_BALCONY;
 assert(balconyPlugDeviceId, 'missing env var PLUG_DEVICE_ID_BALCONY');
+const pythonScriptsFilepath = process.env.PYTHON_SCRIPTS_FILEPATH;
+assert(pythonScriptsFilepath, 'missing env var PYTHON_SCRIPTS_FILEPATH');
 
 logger.info('Running with plug device ids', {
   PLUG_DEVICE_ID_PUMP: pumpPlugDeviceId,
@@ -56,10 +64,37 @@ const stopIrrigationValve = setIrrigationValve({
   valvePlugDeviceIds: [tomatoesPlugDeviceId, balconyPlugDeviceId]
 });
 
+const mongoConnect = require('./lib/model/mongodb-connector')({
+  url: MONGODB_URI
+});
+const mongoRepo = require('./lib/model/water-distance-measurement-mongo-repository')({
+  connect: mongoConnect,
+  validate: validateMeasurementObject
+});
+
+const httpRepo = require('./lib/model/water-distance-measurement-http-repository')({
+  apiToken: API_TOKEN,
+  uri: HTTP_REPO_URI,
+  validate: validateMeasurementObject
+});
+
+const pythonScriptsStr = fs.readFileSync(pythonScriptsFilepath);
+const pythonScripts = JSON.parse(pythonScriptsStr);
+const captureDistance = require('./lib/capture-distance');
+
 const app = require('./lib/app-pi')({
   apiToken: API_TOKEN,
   buildMetadata: buildMetadata,
-  captureDistance: () => Promise.resolve(),
+  captureDistance: function() {
+    captureDistance({
+      exec: exec,
+      pythonScripts: pythonScripts,
+      repos: [
+        httpRepo,
+        mongoRepo
+      ]
+    });
+  },
   plugRepo: plugRepo,
   powerOn: powerOn,
   powerOff: powerOff,
--- test/integration/server-pi.spec.js
+++ test/integration/server-pi.spec.js
@@ -1,17 +1,37 @@
 'use strict';
 
+const bodyParser = require('body-parser');
 const expect = require('chai').expect;
+const express = require('express');
 const fs = require('fs');
 const manageChildProcesses = require('./../util/manage-child-processes');
 const path = require('path');
 const request = require('request');
+const sinon = require('sinon');
 let spawn;
+const tmp = require('tmp');
 
 manageChildProcesses((patchedExec, patchedSpawn) => {
   spawn = patchedSpawn;
 });
 
 describe('server-pi.js', function() {
+  this.timeout(5000);
+  
+  before('start server', function() {
+    const app = express();
+    this.httpSpy = sinon.spy();
+    app.use(bodyParser.json(), (req, res) => {
+      this.httpSpy(req.uri, req.body, req);
+      res.json({});
+    });
+    this.httpServerPort = 3333;
+    this.httpServer = app.listen(this.httpServerPort);
+  });
+  
+  after('stop server', function() {
+    this.httpServer.close();
+  });
   
   beforeEach('set-up build-metadata', function() {
     this.buildMetadataFilepath = path.resolve(__dirname, './../../build-metadata.json');
@@ -32,12 +52,29 @@ describe('server-pi.js', function() {
   beforeEach(function() {
     this.apiToken = '1234';
     this.port = 3010;
+    this.httpRepoUri = `http://localhost:${this.httpServerPort}`;
+    
+    const pythonScriptCwd = path.resolve(__dirname, './../fixture');
+    const pythonScripts = [
+      { name: 'short', command: 'vl53l0x-short-output.py', cwd: pythonScriptCwd},
+      { name: 'long', command: 'vl53l0x-long-output.py', cwd: pythonScriptCwd}
+    ];
+    this.pythonScriptsFilepath = tmp.fileSync().name;
+    fs.writeFileSync(this.pythonScriptsFilepath, JSON.stringify(pythonScripts));
+    
+    this.mongodb_uri = 'mongodb://localhost:27017';
+    this.mongoConnect = require('./../../lib/model/mongodb-connector')({
+      url: this.mongodb_uri
+    });
     
     const command = 'node';
     const args = ['server-pi.js'];
     const env = process.env;
-    env.api_token = this.apiToken;
+    env.API_TOKEN = this.apiToken;
     env.PORT = this.port;
+    env.HTTP_REPO_URI = this.httpRepoUri;
+    env.MONGODB_URI = this.mongodb_uri;
+    env.PYTHON_SCRIPTS_FILEPATH = this.pythonScriptsFilepath;
     const options = {
       cwd: path.resolve(__dirname, './../../'),
       env: env
@@ -58,6 +95,11 @@ describe('server-pi.js', function() {
         });
       });
     })();
+  });
+  
+  afterEach('close mongodb connection', function() {
+    return this.mongoConnect()
+      .then(db => db.close());
   });
   
   describe('endpoints', function() {
