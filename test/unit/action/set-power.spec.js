67e68c15a6defa58d98de2ac4df2208214b2b910
Maximilian Lenkeit <maximilian.lenkeit@sap.com>
2017-08-09 10:47:33 +0200

    refactor(set-power): update plug state in repo

--- test/unit/action/set-power.spec.js
+++ test/unit/action/set-power.spec.js
@@ -10,6 +10,9 @@ describe('action/set-power', function() {
     
     this.plugRepo = require('./../../util/mock-plug-repo')();
     this.plugRepo.findById.withArgs(this.deviceId).resolves(this.plug);
+    
+    this.plugStateRepo = require('./../../util/mock-plug-state-repo')();
+    this.plugStateRepo.upsert.resolves();
   });
   
   context('for value true', function() {
@@ -17,16 +20,32 @@ describe('action/set-power', function() {
     beforeEach(function() {
       this.setPower = require('./../../../lib/action/set-power')({
         plugRepo: this.plugRepo,
+        plugStateRepo: this.plugStateRepo,
         value: true
       });
     });
-  
-    it('resolves the promise after invoking setPowerState with true', function() {
+    
+    it('resolves the promise after invoking plugStateRepo#upsert', function() {
+      return expect(this.setPower(this.deviceId))
+        .to.eventually.be.fulfilled
+        .then(() => {
+          expect(this.plugStateRepo.upsert)
+            .to.be.calledWith({ deviceId: this.deviceId, relayState: 1 /*true*/ });
+        });
+    });
+    
+    it('calls setPowerState on the plug with true', function() {
       return expect(this.setPower(this.deviceId))
         .to.eventually.be.fulfilled
         .then(() => {
           expect(this.plug.setPowerState).to.be.calledWith(true);
         });
+    });
+    
+    it('resolves the promise even if the plug was not found', function() {
+      this.plugRepo.findById.withArgs(this.deviceId).rejects(null);
+      return expect(this.setPower(this.deviceId))
+        .to.eventually.be.fulfilled;
     });
   });
   
@@ -35,16 +54,32 @@ describe('action/set-power', function() {
     beforeEach(function() {
       this.setPower = require('./../../../lib/action/set-power')({
         plugRepo: this.plugRepo,
+        plugStateRepo: this.plugStateRepo,
         value: false
       });
     });
-  
-    it('resolves the promise after invoking setPowerState with false', function() {
+    
+    it('resolves the promise after invoking plugStateRepo#upsert', function() {
+      return expect(this.setPower(this.deviceId))
+        .to.eventually.be.fulfilled
+        .then(() => {
+          expect(this.plugStateRepo.upsert)
+            .to.be.calledWith({ deviceId: this.deviceId, relayState: 0 /*false*/ });
+        });
+    });
+    
+    it('calls setPowerState on the plug with true', function() {
       return expect(this.setPower(this.deviceId))
         .to.eventually.be.fulfilled
         .then(() => {
           expect(this.plug.setPowerState).to.be.calledWith(false);
         });
+    });
+    
+    it('resolves the promise even if the plug was not found', function() {
+      this.plugRepo.findById.withArgs(this.deviceId).rejects(null);
+      return expect(this.setPower(this.deviceId))
+        .to.eventually.be.fulfilled;
     });
   });
 });
