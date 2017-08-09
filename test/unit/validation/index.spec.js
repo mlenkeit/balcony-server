f665bd365b06b3876a2b7d2ab08444d550bc3700
Maximilian Lenkeit <maximilian.lenkeit@sap.com>
2017-08-09 09:53:21 +0200

    feat(validation): validate plug state objects

--- test/unit/validation/index.spec.js
+++ test/unit/validation/index.spec.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const createMeasurementObj = require('./../../fixture/create-measurement-obj');
+const createPlugStateObj = require('./../../fixture/create-plug-state-obj');
 const expect = require('chai').expect;
 
 const validation = require('./../../../lib/validation/index');
@@ -20,31 +21,56 @@ describe('validation', function() {
     
     describe('returns false when', function() {
       
-      it('timestamp propert is missing', function() {
+      it('timestamp property is missing', function() {
         delete this.obj.timestamp;
         expect(this.validate(this.obj)).to.equal(false);
       });
         
-      it('sensor propert is missing', function() {
+      it('sensor property is missing', function() {
         delete this.obj.sensor;
         expect(this.validate(this.obj)).to.equal(false);
       });
         
-      it('measurementType propert is missing', function() {
+      it('measurementType property is missing', function() {
         delete this.obj.measurementType;
         expect(this.validate(this.obj)).to.equal(false);
       });
         
-      it('measurement propert is missing', function() {
+      it('measurement property is missing', function() {
         delete this.obj.measurement;
         expect(this.validate(this.obj)).to.equal(false);
       });
         
-      it('measurementUnit propert is missing', function() {
+      it('measurementUnit property is missing', function() {
         delete this.obj.measurementUnit;
         expect(this.validate(this.obj)).to.equal(false);
       });
       
+    });
+  });
+    
+  describe('validatePlugState', function() {
+    
+    beforeEach(function() {
+      this.validate = validation.validatePlugStateObj;
+      this.obj = createPlugStateObj();
+    });
+    
+    it('returns true for valid data', function() {
+      expect(this.validate(this.obj)).to.equal(true);
+    });
+    
+    describe('returns false when', function() {
+      
+      it('deviceId property is missing', function() {
+        delete this.obj.deviceId;
+        expect(this.validate(this.obj)).to.equal(false);
+      });
+        
+      it('relayState property is missing', function() {
+        delete this.obj.relayState;
+        expect(this.validate(this.obj)).to.equal(false);
+      });
     });
   });
 });
